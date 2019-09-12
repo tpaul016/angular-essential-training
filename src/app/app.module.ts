b8d168f97e81603f8484f8f36a7656061966cb25
Tristan Paul [I] <trpaul@tesla.com>
2019-09-11 18:58:20 -0700

    completed up to 06_02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form.component';
+import { lookupListToken, lookupLists} from './providers';
 
 @NgModule({
-    imports: [
-        BrowserModule
-    ],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [
-        AppComponent
-    ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent,
+  ],
+  providers: [
+    { provide: lookupListToken, useValue: lookupLists}
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
